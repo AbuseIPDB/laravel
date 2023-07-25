38584b5d1d98f27a64b6cb54bd9f2636a180f547
rororowyourkayak <roshanforde@gmail.com>
2023-07-25 14:48:36 -0400

    save changes to the tests

--- tests/TestRequests.php
+++ tests/TestRequests.php
@@ -1,14 +1,28 @@
-<?php 
+<?php
 
-namespace AbuseipdbLaravel\Tests; 
+namespace AbuseipdbLaravel\Tests;
 
 use AbuseipdbLaravel\Tests\TestCase;
-use AbuseipdbLaravel\Facades\AbuseIPDB;
 use Illuminate\Http\Client\Response;
 use AbuseipdbLaravel\ResponseObjects;
+use AbuseipdbLaravel\Facades\AbuseIPDB;
 
-class TestRequests extends TestCase {
+class TestRequests extends TestCase
+{
 
+    public function testIlluminateResponseType()
+    {
+        $response = AbuseIPDB::makeRequest('check', ['ipAddress'=>'127.0.0.1']);
+        $this->assertInstanceOf(\Illuminate\Http\Client\Response::class,$response);
+    }
+    public function testCheckResponseType()
+    {
+        $response = AbuseIPDB::check('127.0.0.1');
+        $this->assertInstanceOf(ResponseObjects\CheckResponse::class,$response);
+    }
+    public function testReportResponseType()
+    {
+        $response = AbuseIPDB::report('127.0.0.2', 21);
+        $this->assertInstanceOf(ResponseObjects\ReportResponse::class,$response);
+    }
 }
-
-?>
