e46ea0fc090e29d120cb8d51e8057186a4550be2
Ash Allen <mail@ashallendesign.co.uk>
2024-02-16 14:58:39 +0000

    Added @see to docblock.

--- src/Facades/AbuseIPDB.php
+++ src/Facades/AbuseIPDB.php
@@ -12,6 +12,8 @@ use AbuseIPDB\AbuseIPDBLaravel;
  * @method static Response|null makeRequest($endpointName, $parameters, $acceptType = 'application/json')
  * @method static CheckResponse check(string $ipAddress, int $maxAgeInDays = 30, bool $verbose = false)
  * @method static ReportResponse report(string $ip, array|int $categories, string $comment = '')
+ *
+ * @see AbuseIPDBLaravel
  */
 class AbuseIPDB extends Facade
 {
