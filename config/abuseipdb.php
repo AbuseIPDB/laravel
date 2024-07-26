7cf5acc4cd4b1430c0c001a2e8399decf8930808
Dimitrios Hrissohoidis <thegreek.dh@gmail.com>
2024-07-26 17:06:58 -0400

    bump vers

--- config/abuseipdb.php
+++ config/abuseipdb.php
@@ -14,5 +14,5 @@ return [
 
     'api_key' => env('ABUSEIPDB_API_KEY'),
     'base_url' => env('ABUSEIPDB_API_BASE_URL', 'https://api.abuseipdb.com/api/v2/'),
-    'version' => '1.0.6',
+    'version' => '1.0.7',
 ];
