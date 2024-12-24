78635aaf6c4c2a491a6f30060191523d7fb79362
Lucas de Araújo Silva <l.de-araujosilva@cyberenergia.com>
2024-12-24 12:06:40 -0300

    style(pint): fixed code style

--- src/AbuseIPDBExceptionReporter.php
+++ src/AbuseIPDBExceptionReporter.php
@@ -7,9 +7,7 @@ use AbuseIPDB\Facades\AbuseIPDB;
 class AbuseIPDBExceptionReporter
 {
     /**
-     * Report a suspicious operation to AbuseIPDB, gathering information from the current request
-     * 
-     * 
+     * Report a suspicious operation to AbuseIPDB, gathering information from the current request.
      */
     public static function reportSuspiciousOperationException(): ResponseObjects\ReportResponse|false
     {
